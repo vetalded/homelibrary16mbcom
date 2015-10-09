f0fe61a1e24d7fb33fd25e1caba55b318c1f7e0a
Oleksandr <yakivchuk.sasha@gmail.com>
2015-10-09 22:40:21 +0300

    создано book_update_dialog.php

--- protected/views/userPage/books.php
+++ protected/views/userPage/books.php
@@ -16,42 +16,42 @@ $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
 ?>
 
 <div class="dialog_window editBook">
-        <form action="" class="bookAddForm">
-            <p>Редагувати книгу</p>
-            <div class="row">
-                <label for="title">Назва:</label>
-                <input type="text" id="title" class="inputB" />
-                <div class="clear"></div>
-            </div>
-            <div class="row">
-                <label for="author">Автор:</label>
-                <input type="text" id="author" class="inputB"/>
-                <div class="clear"></div>
-            </div>
-            <div class="row">
-                <label for="year">Рік:</label>
-                <input type="text" id="year" class="inputB"/>
-                <div class="clear"></div>
-            </div>
-            <div class="row">
-                <label for="state">Стан:</label>
-                <input type="text" id="state" class="inputB"/>
-                <div class="clear"></div>
-            </div>
-            <div class="row">
-                <label for="type">Тип пропозиції:</label>
-                <select name="" id="type" class="bookAddSelect">
-                    <option value="">Тип пропозиції</option>
-                    <option value="1">Продам</option>
-                    <option value="2">Обміняю</option>
-                    <option value="3">Подарую</option>
-                </select>
-                <div class="clear"></div>
-            </div>
-            <input type="button" value="Додати" class="button-style" />
+    <form action="" class="bookAddForm">
+        <p>Редагувати книгу</p>
+        <div class="row">
+            <label for="title">Назва:</label>
+            <input type="text" id="title" class="inputB" />
             <div class="clear"></div>
-        </form>
-
+        </div>
+        <div class="row">
+            <label for="author">Автор:</label>
+            <input type="text" id="author" class="inputB"/>
+            <div class="clear"></div>
+        </div>
+        <div class="row">
+            <label for="year">Рік:</label>
+            <input type="text" id="year" class="inputB"/>
+            <div class="clear"></div>
+        </div>
+        <div class="row">
+            <label for="state">Стан:</label>
+            <input type="text" id="state" class="inputB"/>
+            <div class="clear"></div>
+        </div>
+        <div class="row">
+            <label for="type">Тип пропозиції:</label>
+            <select name="" id="type" class="bookAddSelect">
+                <option value="">Тип пропозиції</option>
+                <option value="1">Продам</option>
+                <option value="2">Обміняю</option>
+                <option value="3">Подарую</option>
+            </select>
+            <div class="clear"></div>
+        </div>
+        <input type="button" value="Додати" class="button-style" />
         <div class="clear"></div>
-    </div>
+    </form>
+
+    <div class="clear"></div>
+</div>
 <?php $this->endWidget('zii.widgets.jui.CJuiDialog'); ?>
